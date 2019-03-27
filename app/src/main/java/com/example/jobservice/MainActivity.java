f1cee2855f75cba1b2430a52ae6cf574b972b009
yulia_norkina <yulia.norkina2@gmail.com>
2019-03-27 11:44:46 +0200

    create UI

--- app/src/main/java/com/example/jobservice/MainActivity.java
+++ app/src/main/java/com/example/jobservice/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.jobservice;
 
+import android.app.SearchManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etNumber;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNumber = findViewById(R.id.etNumber);
+        tvResult = findViewById(R.id.tvResult);
+        findViewById(R.id.bCalculate).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
